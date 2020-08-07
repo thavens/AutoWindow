647ce9b1ab6b8437ad83e9ed1182baa99a41e0db
Michael Lavery <michael.lavery.2017@gmail.com>
2020-08-06 17:35:06 -0700

    live test udpates

--- EspWeatherGrabber/EspWeatherGrabber.ino
+++ EspWeatherGrabber/EspWeatherGrabber.ino
@@ -29,28 +29,44 @@ void setup() {
     ESP.restart();
   }
 
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("myesp8266");
 
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
   ArduinoOTA.onEnd([]() {
     Serial.println("\nEnd");
   });
-
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
-  
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
   });
-  
   ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -91,7 +107,6 @@ void getWeatherData() {
   StaticJsonDocument<1024> json_buf;\
   deserializeJson(json_buf, jsonArray);
   JsonObject root = json_buf.as<JsonObject>();
-
   Temperature = root["main"]["temp"];
   Humidity = root["main"]["humidity"];
   Temp_Max = root["main"]["temp_max"];
