5eae9da076d08fe3fcefdd6afd3b931f7dabc3df
Thavens <michael.lavery.2017@gmail.com>
2021-03-31 19:14:19 -0700

    added move window and data features

--- EspWindow2.0/EspWindow2.0.ino
+++ EspWindow2.0/EspWindow2.0.ino
@@ -1,22 +1,87 @@
+/* 1. create website to change state - Done
+ * 2. read in state change through API - Done
+ * 3. open and close window - Done
+ * 4. add inside temperature to the website 
+ *    - import - done
+ *    - initialize - done
+ *    - find data - done
+ *    - report - done
+ * 5. get outside temperature ever ten minutes
+ *    - variables - done
+ *    - get request - done
+ *    - retport - done
+ */
+ 
+#include <ArduinoJson.h>
+#include <DHT.h>;
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid     = "ssid";
-const char* password = "password";
+#define dirPin 13     //stepper motor direction pin
+#define stepPin 12    //pin to indicate step on motor
+#define ENAPin 11     //stepper motor ative pin
+#define LIMIT 5       //limit switch pin connected to normally closed # true = high = opened, false = low = closed
+#define DHTPin 4     // tempsensor pin (inside house)
+#define DHTType DHT22   // DHT 22  (AM2302)
 
+DHT dht(DHTPin, DHTType); // Initialize DHT sensor
 ESP8266WebServer server(80);
 
-String header;
+const char* ssid     = "ssid";
+const char* password = "password";
+const String APIKEY = "Open Weather API Key";                                 
+const String ZIPCode = "ZIP";   //ZIPCode
+float outTemp;
+float humidity;
+float tempMin;
+float tempMax;
+String servername="api.openweathermap.org";              // remote server program will connect to
+String result;
+unsigned long oldTime = millis() - 60000;
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+//############################[html code]######################################
+
+const char* html = "<!DOCTYPE html>\
+<html>\
+<head>\
+    <script>\
+        function sendOpen() {\
+            var xhr = new XMLHttpRequest();\
+            xhr.open('POST', 'http://192.168.1.18/change', true);\
+            xhr.setRequestHeader('Content-Type', 'application/json');\
+            xhr.send('open')\
+        }\
+        function sendClose() {\
+            var xhr = new XMLHttpRequest();\
+            xhr.open('POST', 'http://192.168.1.18/change', true);\
+            xhr.setRequestHeader('Content-Type', 'application/json');\
+            xhr.send('close')\
+        }\
+    </script>\
+</head>\
+<body>\
+    <h1> ESP window </h1>\
+    <p>Window is: %s</p>\
+    <p id='data'>Indoor Temp: %f&#8457, Outdoor Temp: %f&#8457, High Temp: %f&#8457, Low Temp: %f&#8457</p>\
+    <button type='button' onClick=sendOpen()>Open Window</button>\
+    <button type='button' onClick=sendClose()>Close Window</button>\
+</body>\
+</html>";
+
+//###########################[end of html]##################################
 
 void setup() {
   Serial.begin(115200);
+  dht.begin();
+
+  pinMode(LIMIT, INPUT_PULLUP);
+
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(ENAPin, OUTPUT);
+
+  digitalWrite(dirPin, HIGH);
+  digitalWrite(ENAPin, HIGH);
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -38,50 +103,140 @@ void setup() {
 }
 
 void loop() {
+  if(millis() > oldTime + 600000) {
+    getWeatherData();
+    oldTime = millis();
+  }
   server.handleClient();
 }
 
+//&#8457 is html-code for degree farenheit
 void handleRoot() {
-  //char temp[400];
-  String temp = "<!DOCTYPE html>\
-<html>\
-<head>\
-    <script>\
-        function sendOpen() {\
-            var xhr = new XMLHttpRequest();\
-            xhr.open('POST', 'http://192.168.1.18/change', true);\
-            xhr.setRequestHeader('Content-Type', 'application/json');\
-            xhr.send('open')\
-        }\
-        function sendClose() {\
-            var xhr = new XMLHttpRequest();\
-            xhr.open('POST', 'http://192.168.1.18/change', true);\
-            xhr.setRequestHeader('Content-Type', 'application/json');\
-            xhr.send('close')\
-        }\
-    </script>\
-</head>\
-<body>\
-    <p> ESP window </p>\
-    <p id='data'></p>\
-    <button type='button' onClick=sendOpen()>Open Window</button>\
-    <button type='button' onClick=sendClose()>Close Window</button>\
-<script>\
-    document.getElementById('data').innerHTML = 'data here'\
-</script>\
-</body>\
-</html>";
-  server.sendContent(temp);
+  int temp = dht.readTemperature(true);
+  char buff[840];
+  char* state;
+  if(digitalRead(LIMIT)) {
+    state = "opened";
+  }
+  else {
+    state = "closed";
+  }
+  sprintf(buff, html, state, temp, outTemp, tempMax, tempMin);
+  
+  server.sendContent(buff);
 }
 
 void handleForm() {
   String state = "";
   if (server.args()) {
     state = server.arg(0);
-    server.send(200, "text/plain", "state change");
   }
   else {
     server.send(400);
+    return;
   }
+  if(state.indexOf("close") >= 0 && digitalRead(LIMIT)) {
+    closeWindow();
+  }
+  else if(state.indexOf("open") >= 0 && !digitalRead(LIMIT)) {
+    openWindow();
+  }
+  else {
+    server.send(400, "text/plain", "can't change");  //attempt to open or close when already in that state
+    return;
+  }
+  server.send(200, "text/plain", "state changed");
   Serial.println(state);
+}
+
+//starts with a slow acceleration of steppermotor to prevent steploss and high forces
+void openWindow() {
+  Serial.println("Opening Window");
+  digitalWrite(ENAPin, LOW);
+  digitalWrite(dirPin, HIGH);
+  for (int i = 0; i < 400; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(800 - (i * 0.75));
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(800 - (i * 0.75));
+  }
+  for (int i = 0; i < 2000; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500 - (i * 0.15625));
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500 - (i * 0.15625));
+  }
+  //7400
+  for (int i = 0; i < 7400; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(200);
+  }
+  digitalWrite(ENAPin, HIGH);
+}
+
+//closes window using limit switch to reduce grinding and user inaccuracy
+void closeWindow() {
+  Serial.println("Closing Window");
+  digitalWrite(ENAPin, LOW);
+  digitalWrite(dirPin, LOW); //set direction as close
+  //begin accelleration of window while checking limit switch
+  for (int i = 0; i < 400 && digitalRead(LIMIT) == HIGH; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(800 - (i * 0.75));
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(800 - (i * 0.75));
+  }
+  for (int i = 0; i < 2000 && digitalRead(LIMIT) == HIGH; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500 - (i * 0.15625));
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500 - (i * 0.15625));
+  }
+  //while checking limit switch move to high speed
+  while(digitalRead(LIMIT) == HIGH) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(200);
+  }
+  digitalWrite(ENAPin, HIGH);
+}
+
+void getWeatherData() {
+  WiFiClient client;
+  if(client.connect(servername, 80)) {
+    client.println("GET /data/2.5/weather?zip="+ZIPCode+"&units=Imperial&APPID="+APIKEY);
+    client.println("Host: api.openweathermap.org");
+    client.println("User-Agent: ArduinoWiFi/1.1");
+    client.println("Connection: close");
+    client.println();
+  }
+  else {
+    Serial.println("There was an error with getting data from open weather");
+    return;
+  }
+
+  while(client.connected() && !client.available()) {
+    delay(10);
+  }
+  while(client.connected() || client.available()) {
+    char c = client.read();
+    result += c;
+  }
+  client.stop();
+  result.replace('[', ' ');
+  result.replace(']', ' ');
+  char jsonArray [result.length()+1];
+  result.toCharArray(jsonArray,sizeof(jsonArray));
+  jsonArray[result.length() + 1] = '\0';
+  StaticJsonDocument<1024> json_buf;\
+  deserializeJson(json_buf, jsonArray);
+  JsonObject root = json_buf.as<JsonObject>();
+  outTemp = root["main"]["temp"];
+  humidity = root["main"]["humidity"];
+  tempMax = root["main"]["temp_max"];
+  tempMin = root["main"]["temp_min"];
+  result = "";
 }
