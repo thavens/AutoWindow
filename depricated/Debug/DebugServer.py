b24067525c48a5bd8888d83d4f7e6abe52c4d876
Thavens <michael.lavery.2017@gmail.com>
2021-04-02 15:00:40 -0700

    old

---
+++ depricated/Debug/DebugServer.py
@@ -0,0 +1,49 @@
+from socket import *
+from datetime import datetime
+import threading
+import traceback
+import sys
+
+ip = 'IP'
+port = 'Port Number' 
+
+buff = []
+def createServer():
+    with socket(AF_INET, SOCK_STREAM) as serversocket:
+        serversocket.bind((ip, int(port)))
+        serversocket.listen(5)
+        while True:
+            try:
+                (client, address) = serversocket.accept()
+
+                client.recv(5000).decode('cp1252')
+                dt = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+                rd = dt + ': ' + client.recv(5000).decode('cp1252')
+                print(rd)
+                buff.append(rd)
+
+                data = 'HTTP/1.1 200 OK\r\n'
+                data += 'Content-Type: text/html; charset=utf-8\r\n'
+                data += '\r\n'
+                data += 'OK\r\n\r\n'
+
+                client.sendall(data.encode())
+                client.shutdown(SHUT_WR)
+            except Exception as e:
+                print('server thread closed')
+                track = traceback.format_exc()
+                print(track)
+                #I know I am the most lazy person in the world.
+                #I hope u like it
+                createServer()
+
+
+sthread = threading.Thread(target=createServer, daemon=True)
+sthread.start()
+
+while True:
+    stop = input('input y to stop anytime\n\n\n')
+    if stop.lower() == 'y':
+        with open('data.txt', 'a') as file:
+            file.writelines(buff)
+        sys.exit(0)
---
+++ depricated/UnoAutoWindow/UnoAutoWindow.ino
@@ -0,0 +1,204 @@
+#include <DHT.h>;
+
+#define dirPin 13     //stepper motor direction pin
+#define stepPin 12    //pin to indicate step on motor
+#define ENAPin 11     //stepper motor ative pin
+#define DHTPin 4     // tempsensor pin (inside house)
+#define DHTType DHT22   // DHT 22  (AM2302)
+#define LIMIT 5       //limit switch pin connected to normally closed pin of limit switch (HIGH is open window LOW is closed window)
+#define userOpen 2    //Allow the user to open window (toggle open tactile button)
+#define userClose 3   //Allow user to close the window (toggle close)
+//hitting open or close first will cause manual overide into said open close state. hitting either open or close (doesn't matter) will reset window to automode
+
+DHT dht(DHTPin, DHTType); // Initialize DHT sensor for normal 16mhz Arduino
+
+String inputString = "";
+bool stringComplete = false;
+bool forceOpen = false;
+bool forceClose = false;
+
+float temp; //internal temperature
+float outTemp; //external temperature
+float Humidity;
+float tempMax;
+float tempMin;
+
+bool isOpen;
+
+unsigned long waitTime = -720000UL;
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+  Serial.println("started");
+
+  pinMode(userOpen, INPUT);
+  pinMode(userClose, INPUT);
+  
+  pinMode(LIMIT, INPUT_PULLUP);
+  
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(ENAPin, OUTPUT);
+  
+  digitalWrite(dirPin, HIGH);
+  digitalWrite(ENAPin, HIGH);
+
+  attachInterrupt(digitalPinToInterrupt(userOpen), userCtrl, RISING);
+  attachInterrupt(digitalPinToInterrupt(userClose), userCtrl, RISING);
+}
+
+void loop()
+{
+  isOpen = digitalRead(LIMIT) == HIGH;
+  
+  if(forceOpen && forceClose) {
+    forceOpen = false;
+    forceClose = false;
+  }
+  
+  //user override mode
+  if(forceOpen) {
+    if(!isOpen) {
+      openWindow();
+    }
+  }
+  else if(forceClose) {
+    if(isOpen) {
+      closeWindow();
+    }
+  }
+  //check string completion
+  else if (stringComplete && inputString.length() > 20 && inputString.length() < 25) {
+    interpData();
+    
+    Serial.println("Outside temp: " + String(outTemp) + "\tInside temp: " +
+                  String(temp) + "\tMin: " + String(tempMin) + "\tMax: " + String(tempMax));
+    Serial.print("Window Open: ");
+    if(isOpen) {
+      Serial.println("True");
+    }
+    else {
+      Serial.println("False");
+    }
+
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+
+    if (millis() - waitTime > 720000UL && outTemp != 0.00) {
+      if(!isOpen && ((tempMax > 76 && outTemp <= temp && temp > 66)
+      || (tempMax < 68 && outTemp > temp)
+      || (tempMax <= 76 && tempMax >= 68 && ((outTemp >= 68 && outTemp <= 76) || (temp >= 68 || temp >= 74))))) {
+        openWindow();
+        waitTime = millis();
+        serialDump();
+      }
+      else if(isOpen && ((tempMax < 68 && outTemp <= temp)
+      || (tempMax >= 80 && (outTemp > temp || temp < 66))
+      || (68 <= tempMax && tempMax <= 76 && (68 > outTemp || outTemp < 76) && temp > 68 && temp < 74))) {
+        closeWindow();
+        waitTime = millis();
+        serialDump();
+      }
+    }
+  }
+  else if (stringComplete) {
+    Serial.println("There was an error in the esp 8266");
+    Serial.println(inputString);
+    inputString = "";
+    stringComplete = false;
+    serialDump();
+  }
+  
+  delay(200);
+}
+
+void interpData() {
+  outTemp = inputString.substring(0,inputString.indexOf(' ')).toFloat();
+  inputString = inputString.substring(inputString.indexOf(' ')+1);
+  Humidity = inputString.substring(0,inputString.indexOf(' ')).toFloat();
+  inputString = inputString.substring(inputString.indexOf(' ')+1);
+  tempMin = inputString.substring(0,inputString.indexOf(' ')).toFloat();
+  inputString = inputString.substring(inputString.indexOf(' ')+1);
+  tempMax = inputString.toFloat();
+  temp = dht.readTemperature(true);
+}
+
+//recieve string data from esp8266 arduino
+//ran by arduino after every loop
+void serialEvent() { 
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    if(inChar != '\n') {
+    inputString += inChar;
+    } else {
+      stringComplete = true;
+    }
+  }
+}
+
+//starts with a slow acceleration of steppermotor to prevent steploss and high forces
+void openWindow() {
+  Serial.println("Opening Window");
+  digitalWrite(ENAPin, LOW);
+  digitalWrite(dirPin, HIGH);
+  for (int i = 0; i < 600; i++) {
+    int d = 195 - 135 * (((double)i)/600);
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(d);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(d);
+  }
+  for (unsigned int i = 0; i < 5.65 * 6400; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(60);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(60);
+  }
+  digitalWrite(ENAPin, HIGH);
+}
+
+//closes window using limit switch to reduce grinding and user inaccuracy
+void closeWindow() {
+  Serial.println("Closing Window");
+  digitalWrite(ENAPin, LOW);
+  digitalWrite(dirPin, LOW); //set direction as close
+  //begin accelleration of window while checking limit switch
+  for (int i = 0; i < 600 && digitalRead(LIMIT) == HIGH; i++) {
+    int d = 195 - 135 * (((double)i)/600);
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(d);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(d);
+  }
+  //close while checking limit switch
+  while(digitalRead(LIMIT) == HIGH) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(60);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(60);
+  }
+  digitalWrite(ENAPin, HIGH);
+}
+
+void serialDump() {
+  while(Serial.available()) {
+    Serial.read();
+  }
+}
+
+void userCtrl() {
+  Serial.println("usercontrol triggered");
+  if(digitalRead(userClose)) {
+    forceClose = !forceClose;
+    Serial.println("user close");
+  }
+  else if(digitalRead(userOpen)) {
+    forceOpen = !forceOpen;
+    Serial.println("user open");
+  }
+}
